10a855dff3430307f3d800ae19ba7f8a68f04e0b
Nekonyann <lymhyd@outlook.com>
2024-10-30 13:05:02 +0800

    * init 初始化

--- src/assets/icons/index.js
+++
@@ -1,7 +0,0 @@
-import Vue from "vue";
-import SvgIcon from "@/components/svgLoader/Index.vue";
-//全局注册组件
-Vue.component("svg-icon", SvgIcon);
-const requireAll = (r) => r.keys().map(r);
-// 加载目录下的所有的 svg 文件
-requireAll(require.context("./svg", false, /\.svg$/));
